95f243e7fb4c371782e62de3c30e9b707819a7f1
BBAtro <drerzhik@mail.ru>
2023-10-21 22:50:39 +0600

    DATABASE LETS GOOOO

--- prometheus/settings.py
+++ prometheus/settings.py
@@ -73,10 +73,15 @@ WSGI_APPLICATION = 'prometheus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'prometheus_db',
+        'USER': 'root',
+        'PASSWORD': 'erz123',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
