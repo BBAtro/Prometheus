1ed0240d67f65ebf90966539f3c92f4c19d12cbc
BBAtro <drerzhik@mail.ru>
2023-10-30 00:02:46 +0600

    Admin = God

--- book_depository/admin.py
+++ book_depository/admin.py
@@ -16,6 +16,6 @@ class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ['author']
     #The author field is now displayed by a search widget, which will be more acceptable than selecting from a dropdown list when you have thousands of users. This is achieved with the raw_id_fields attribute
     date_hierarchy = 'publish'
-    #Just below the search bar are navigation links to navigate through the date hierarchy; this is defined by the date_hierarchy attribute(Add Menu)
+    #Just below the search bar are navigation links to navigate through the date hierarchy; this is defined by the date_hierarchy attribute(Add Menu) 
     ordering = ['status', 'publish']
     #You can also see that by default, posts are ordered by STATUS and PUBLISH columns. The ordering attribute has been used to the sorting criteria that will be used by default
